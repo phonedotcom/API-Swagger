861005b7f8c385c0804eeea75a7c57017a780b8d
Igor Simevski <i.simevski@gmail.com>
2017-02-26 23:41:11 +0100

    bug fix: config file retrieval in go

--- phonecom-cli/src/phonecom/phonecom.go
+++ phonecom-cli/src/phonecom/phonecom.go
@@ -8,12 +8,22 @@ import (
   "phonecom-go-sdk"
   "errors"
 	"strconv"
+  "path/filepath"
+  "log"
 )
 
 var configPath = "config.xml" // Used as a variable. To be changed in tests.
 var param CliParams
 
 func main() {
+
+  dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+  if err != nil {
+    log.Fatal(err)
+    os.Exit(0)
+  }
+
+  configPath = dir + "/config.xml"
 
 	app := cli.NewApp()
 
